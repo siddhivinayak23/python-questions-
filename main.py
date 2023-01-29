e194068c2f711306f774cf7388ff8b4d16ed5fd1
siddhivinayak naik <121494450+siddhivinayak23@users.noreply.github.com>
2023-01-29 10:00:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+'''Write a python program to read the age of a candiadte and detrermine whether it is eligible for casting his/her own vote'''
+
+vote_age = int(input("Enter your age: "))
+
+if vote_age > 18:
+  print(
+    f"Congratulastion! You are age {vote_age} eligible for casting your vote")
+else:
+  print(f"Sorry, You are age {vote_age} less for casting vote.")
